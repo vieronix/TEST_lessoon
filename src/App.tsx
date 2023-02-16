bad5cc1232385e07aaf94c1c11801fb055c5214c
{vier} <{mort1p@yandex.ru}>
2023-02-16 11:48:49 +0700

    forst component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from "./companents/Hello_World";
+import Name from "./companents/Name";
+
 
 function App() {
-  return (
+    return (
 
 
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="App">
+            <header className="App-header">
+                <HelloWorld></HelloWorld>
+                <Name></Name>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+
 }
 
 export default App;
---
+++ src/companents/Name.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Name = () => {
+    return (
+        <div>
+            Malahov Dmitriy
+        </div>
+    );
+};
+
+export default Name;
